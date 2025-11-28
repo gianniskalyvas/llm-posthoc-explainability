e8e0529ef7a3031fb0eb908fdfe68db188b71f4f
gianniskalyvas <gian.kalyva@gmail.com>
2025-11-29 00:01:27 +0200

    .

--- crest/my_maskers_train.sh
+++ crest/my_maskers_train.sh
@@ -1,4 +1,8 @@
 
-python3 rationalizers train --config configs/masker/my_movies_longt5.yaml --seed 0
+#python3 rationalizers train --config configs/masker/my_movies_longt5.yaml --seed 0
 
-python3 rationalizers train --config configs/masker/my_esnli_baset5.yaml --seed 0
+#python3 rationalizers train --config configs/masker/my_esnli_baset5.yaml --seed 0
+
+python3 rationalizers train --config configs/editor/my_movies_longt5.yaml --seed 0
+
+python3 rationalizers train --config configs/editor/my_esnli_baset5.yaml --seed 0
