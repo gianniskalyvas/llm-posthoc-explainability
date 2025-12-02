e72a329199580361670329738b768ea2cea83c3d
gianniskalyvas <gian.kalyva@gmail.com>
2025-12-03 01:12:14 +0200

    .

--- crest/editors_train.sh
+++ crest/editors_train.sh
@@ -3,7 +3,6 @@ python3 rationalizers train --config configs/editor/imdb_sparsemap_50p.yaml --se
 # >>> experiments/masker_imdb_sparsemap_50p/versionNone/checkpoints/epoch=5.ckpt
 
 # 1.2) Train Editor
-python3 rationalizers train --config configs/masker/snli_sparsemap_30p.yaml --seed 0
 python3 rationalizers train --config configs/editor/imdb_sparsemap_50p.yaml --seed 0
 
 # >>> experiments/masker_snli_sparsemap_30p/versionNone/checkpoints/epoch=10.ckpt
