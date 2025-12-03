b7e5e33c0fb45bce1eedb020d30175f0fa99e699
gianniskalyvas <gian.kalyva@gmail.com>
2025-12-03 17:57:17 +0200

    .

--- crest/imdb_train.sh
+++ crest/imdb_train.sh
@@ -1,6 +1,6 @@
 # 1.1) Train Masker
-python3 rationalizers train --config configs/masker/imdb_sparsemap_50p.yaml --seed 0
+python3 rationalizers train --config configs/masker/my_movies_sparsemap_50p_longT5.yaml --seed 0
 # >>> experiments/masker_imdb_sparsemap_30p/versionjkexuek0/checkpoints/epoch=1.ckpt
 
 # 1.2) Train Editor
-python3 rationalizers train --config configs/editor/imdb_sparsemap_50p.yaml --seed 0
+python3 rationalizers train --config configs/editor/my_movies_sparsemap_50p_longT5.yaml --seed 0
